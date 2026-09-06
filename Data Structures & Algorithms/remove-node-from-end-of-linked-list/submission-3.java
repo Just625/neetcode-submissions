341aa7a8c358a9a07ce34aba315fc431fba2f37c
Just625 <60479882+Just625@users.noreply.github.com>
2026-09-06 22:29:43 +0700

    Add: remove-node-from-end-of-linked-list - submission-3

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-3.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // // Time O(n), Space O(n)
+        // List<ListNode> list = new ArrayList<>();
+        // ListNode cur = head;
+        // while (cur != null) {
+        //     list.add(cur);
+        //     cur = cur.next;
+        // }
+
+        // int size = list.size();
+
+        // if (size == 1) return null;
+
+        // // Remove head
+        // if (n == list.size()) {
+        //     return head.next;
+        // }
+
+        // // Remove tail
+        // // if (n == 1) {
+        // //     list.get(size - n - 1).next = null;
+        // //     return head;
+        // // }
+
+        // // Connect node before n and after n
+        // list.get(size - n - 1).next = list.get(size - n).next;
+        // return head;
+
+        // Time O(n), Space O(1)
+        int size = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            size++;
+            cur = cur.next;
+        }
+
+        // Remove head
+        if (n == size) return head.next;
+
+        cur = head;
+        for (int i = 0; i < size - n; i++) {
+            if (i == size - n - 1) {
+                cur.next = cur.next.next;
+            }
+            cur = cur.next;
+        }
+
+        return head;
+    }
+}
