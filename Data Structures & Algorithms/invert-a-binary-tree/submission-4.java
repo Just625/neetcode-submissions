9ff605c4f1df1b77feb8df171276fcc8d184404d
Just625 <60479882+Just625@users.noreply.github.com>
2026-09-08 10:11:02 +0700

    Add: invert-a-binary-tree - submission-4

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-4.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        // BFS: Time O(n), Space O(n)
+        // if (root == null) return root;
+
+        // Using queue interface is better then deque here
+        // Queue<TreeNode> queue = new ArrayDeque<>();
+        // queue.offer(root);
+
+        // while (!queue.isEmpty()) {
+        //     TreeNode cur = queue.poll();
+
+        //     // if (cur == null) continue;
+
+        //     TreeNode temp = cur.left;
+        //     cur.left = cur.right;
+        //     cur.right = temp;
+
+        //     if (cur.left != null) queue.offer(cur.left);
+        //     if (cur.right != null) queue.offer(cur.right);
+        // }
+
+        // return root;
+
+        // DFS: Time O(n), Space O(n)
+        if (root == null) return root;
+
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.push(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.pop();
+
+            TreeNode temp = cur.left;
+            cur.left = cur.right;
+            cur.right = temp;
+
+            if (cur.left != null) queue.push(cur.left);
+            if (cur.right != null) queue.push(cur.right);
+        }
+
+        return root;
+    }
+}
