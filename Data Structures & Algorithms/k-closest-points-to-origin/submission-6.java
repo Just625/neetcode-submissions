3d5d6493e4ee1cc1df8efe4d50d9a90121db18e5
Just625 <60479882+Just625@users.noreply.github.com>
2026-09-11 15:16:15 +0700

    Add: k-closest-points-to-origin - submission-6

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-6.java
@@ -0,0 +1,95 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        // int[][] ans = new int[k][2];
+        // Map<Double, List<Integer>> distanceMap = new HashMap<>();
+
+        // for (int i = 0; i < points.length; i++) {
+        //     int[] arr = points[i];
+        //     int x = arr[0];
+        //     int y = arr[1];
+        //     distanceMap.computeIfAbsent(calculateDistance(x, y), e -> new ArrayList<>()).add(i);
+        // }
+
+        // List<Double> distances = new ArrayList<>(distanceMap.keySet());
+        // distances.sort(Double::compare);
+
+        // int i = 0;
+        // for (double distance : distances) {
+        //     List<Integer> indices = distanceMap.get(distance);
+        //     // System.out.println("indices:" + indices + ", i: " + i + ",k: " + k);
+        //     for (int idx : indices) {
+        //         if (k <= 0) break;
+        //         ans[i] = points[idx];
+        //         i++;
+        //         k--;
+        //     }
+        // }
+        // return ans;
+
+
+        // int[][] ans = new int[k][2];
+        // Queue<int[]> minHeap = new PriorityQueue<>((a, b) -> Double.compare(calculateDistance(a[0], a[1]), calculateDistance(b[0], b[1])));
+
+        // for (int[] point : points) {
+        //     minHeap.offer(point);
+        // }
+
+        // int i = 0;
+        // while (!minHeap.isEmpty() && i < k) {
+        //     ans[i] = minHeap.poll();
+        //     i++;
+        // }
+
+        // return ans;
+
+        // Time O(nlogk + klogk) -> O((n+k)logk) -> O(2nlogk) -> O(nlogk)?
+        // Space O(k)
+        // int[][] ans = new int[k][2];
+        // Queue<int[]> maxHeap = new PriorityQueue<>((a, b) -> Double.compare(calculateDistance(b[0], b[1]), calculateDistance(a[0], a[1])));
+
+        // for (int[] point : points) {
+        //     maxHeap.offer(point);
+        //     if (maxHeap.size() > k) {
+        //         maxHeap.poll();
+        //     }
+        // }
+
+        // int i = 0;
+        // while (!maxHeap.isEmpty()) {
+        //     ans[i] = maxHeap.poll();
+        //     i++;
+        // }
+
+        // return ans;
+
+        int[][] ans = new int[k][2];
+        // array format: [x, y, distance]
+        // Queue<int[]> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b[2], a[2]));
+        Queue<int[]> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b[0] * b[0] + b[1] * b[1], a[0] * a[0] + a[1] * a[1]));
+
+        for (int[] point : points) {
+            int x = point[0], y = point[1];
+            // No need to use Math.sqrt here, since if x < y then x^1/2 < y^1/2 when x,y, > 0
+            // maxHeap.offer(new int[]{x, y, x*x + y * y});
+            maxHeap.offer(point);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+
+        int i = 0;
+        while (!maxHeap.isEmpty()) {
+            // int[] node = maxHeap.poll();
+            // ans[i][0] = node[0];
+            // ans[i][1] = node[1];
+            ans[i] = maxHeap.poll();
+            i++;
+        }
+
+        return ans;
+    }
+
+    public double calculateDistance(int x, int y) {
+        return Math.sqrt(x * x + y * y);
+    }
+}
