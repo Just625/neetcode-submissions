3b3d9d3ed170614c9f95d647c8d551eedd7fa3a4
Just625 <60479882+Just625@users.noreply.github.com>
2026-09-14 21:54:03 +0700

    Add: same-binary-tree - submission-3

---
+++ Data Structures & Algorithms/same-binary-tree/submission-3.java
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // Time O(n), Space O(w) where w is the maximum width of the tree
+        // // BFS throught two trees at the same time
+        // // If values are not equals, return false right away
+        // // Check if both queues are empty, return true, else return false
+        // if (p == null && q == null) return true;
+        // if (p == null && q != null
+        //     || (p != null && q == null)) return false;
+
+        // Queue<TreeNode> firstQueue = new ArrayDeque<>();
+        // firstQueue.offer(p);
+        // Queue<TreeNode> secondQueue = new ArrayDeque<>();
+        // secondQueue.offer(q);
+
+        // while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+        //     TreeNode first = firstQueue.poll();
+        //     TreeNode second = secondQueue.poll();
+
+        //     if (first.val != second.val) {
+        //         return false;
+        //     }
+
+        //     if (first.left != null && second.left != null) {
+        //         firstQueue.offer(first.left);
+        //         secondQueue.offer(second.left);
+        //     } else if (first.left == null && second.left != null
+        //     || (first.left != null && second.left == null)) {
+        //         return false;
+        //     }
+
+        //     if (first.right != null && second.right != null) {
+        //         firstQueue.offer(first.right);
+        //         secondQueue.offer(second.right);
+        //     } else if (first.right == null && second.right != null
+        //     || (first.right != null && second.right == null)) {
+        //         return false;
+        //     }
+        // }
+
+        // return firstQueue.isEmpty() && secondQueue.isEmpty();
+
+
+        // Timne O(n), Space O(n)
+        if (p == null && q == null) return true;
+        // else if (p == null && q != null
+        //     || (p != null && q == null)) return false;
+        // else if (p.val != q.val) return false;
+
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+
+        // boolean isSameTreeLeft = isSameTree(p.left, q.left);
+        // boolean isSameTreeRight = isSameTree(p.right, q.right);
+
+        // return isSameTreeLeft && isSameTreeRight;
+        // Short circut
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
